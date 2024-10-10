4bbafb3c7b7f54dfcc239772761fe6678668f335
jugstalt <gstalt@gmx.at>
2024-10-10 16:02:37 +0200

    upgrade .NET8

--- src/LiveShareHub/Startup.cs
+++ src/LiveShareHub/Startup.cs
@@ -1,6 +1,3 @@
-using Certes;
-using FluffySpoon.AspNet.EncryptWeMust;
-using FluffySpoon.AspNet.EncryptWeMust.Certes;
 using LiveShareHub.Core.Extensions.DependencyInjection;
 using LiveShareHub.Core.Services;
 using LiveShareHub.Hubs;
@@ -11,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace LiveShareHub
 {
@@ -42,46 +38,6 @@ namespace LiveShareHub
             {
                 config.EncryptionPassword = Configuration["Crypto:KeyPassword"];
             });
-
-            #region FluffySpoon Letsencrypt
-
-            if (Configuration["Letsencrypt:Use"]?.ToLower() == "true")
-            {
-                services.AddFluffySpoonLetsEncrypt(new LetsEncryptOptions()
-                {
-                    Email = Configuration["Letsencrypt:Email"],
-                    UseStaging = false, //switch to true for testing
-                    Domains = new[] { Configuration["Letsencrypt:Domain"] },
-                    TimeUntilExpiryBeforeRenewal = TimeSpan.FromDays(30), //renew automatically 30 days before expiry
-                    TimeAfterIssueDateBeforeRenewal = TimeSpan.FromDays(7), //renew automatically 7 days after the last certificate was issued
-                    CertificateSigningRequest = new CsrInfo() //these are your certificate details
-                    {
-                        CountryName = Configuration["Letsencrypt:CrsInfo:CountryName"],
-                        Locality = Configuration["Letsencrypt:CrsInfo:Locality"],
-                        Organization = Configuration["Letsencrypt:CrsInfo:Organization"],
-                        OrganizationUnit = Configuration["Letsencrypt:CrsInfo:OrganizationUnit"],
-                        State = Configuration["Letsencrypt:CrsInfo:State"]
-                    }
-                });
-
-                if (!String.IsNullOrEmpty(Configuration["Letsencrypt:PersistancePath"]))
-                {
-                    System.IO.Directory.CreateDirectory(System.IO.Path.Combine(Configuration["Letsencrypt:PersistancePath"], "certs"));
-                    System.IO.Directory.CreateDirectory(System.IO.Path.Combine(Configuration["Letsencrypt:PersistancePath"], "certs_Account"));
-                    System.IO.Directory.CreateDirectory(System.IO.Path.Combine(Configuration["Letsencrypt:PersistancePath"], "challenge"));
-
-                    //the following line tells the library to persist the certificate to a file, so that if the server restarts, the certificate can be re-used without generating a new one.
-                    services.AddFluffySpoonLetsEncryptFileCertificatePersistence(System.IO.Path.Combine(Configuration["Letsencrypt:PersistancePath"], "certs"));
-                    services.AddFluffySpoonLetsEncryptFileChallengePersistence(System.IO.Path.Combine(Configuration["Letsencrypt:PersistancePath"], "challenge"));
-                }
-                else
-                {
-                    //the following line tells the library to persist challenges in-memory. challenges are the "/.well-known" URL codes that LetsEncrypt will call.
-                    services.AddFluffySpoonLetsEncryptMemoryChallengePersistence();
-                }
-            }
-
-            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -90,11 +46,6 @@ namespace LiveShareHub
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-
-            if (Configuration["Letsencrypt:Use"]?.ToLower() == "true")
-            {
-                app.UseFluffySpoonLetsEncrypt();
             }
 
             //app.UseHttpsRedirection();
